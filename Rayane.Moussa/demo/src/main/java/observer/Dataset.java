47430051aa7e622275983e8f46f9a3a44d483692
Iheb-Mouldi MOUSSA <iheb.mouldi.moussa@gmail.com>
2022-06-04 02:56:07 +0200

    Update with Observable and Observer

--- Rayane.Moussa/demo/src/main/java/observer/Dataset.java
+++ Rayane.Moussa/demo/src/main/java/observer/Dataset.java
@@ -2,21 +2,36 @@ package observer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
 
 import javax.xml.crypto.Data;
 
-public class Dataset {
+public class Dataset extends Observable {
    
 
     private List<BaseClass> objects = new ArrayList<>();
+    private List<Observer> observers = new ArrayList<>();
+    
     public void add(BaseClass baseClass)
     {
         this.objects.add(baseClass);
-        if(baseClass instanceof Image)
+        if(baseClass instanceof Image) // || Audio
         {
-            System.out.println("Image object");   
+            notifyObservers();
+            // NOTIFY PLAYER OBSERVER
+            //System.out.println("Image object");   
+        }
+        else 
+        {
+          //  
         }
         System.out.println("New Objects has been added to the collection");
+    }
+
+    public void register(Observer observer)
+    {
+
     }
 
     public static void main(String[] args) {
